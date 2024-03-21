985c88a80483ca0ed3f0e19a856b6c87a03a16f7
chaabane-fares <chaabanefares3@gmail.com>
2024-03-21 03:11:26 +0100

    i have modified the login page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,6 @@ import { useAppSelector } from '@src/modules/shared/store'
 import { Helmet } from 'react-helmet-async'
 import { useTranslation } from 'react-i18next'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { supabase } from '@src/client'
-import { useState, useEffect } from 'react'
 
 const App = () => {
   const queryClient = new QueryClient()
@@ -13,38 +11,6 @@ const App = () => {
 
   const theme = useAppSelector((state) => state.theme.mode)
 
-  {
-    /* // ******check user with supabase*****
-  const[user,setUser]=useState(null)
-  useEffect(()=>{
-    checkUser();
-    //****to we can detect when the OAuth redirect is happened and then  recalling check user on redirect ****
-    window.addEventListener('hashchange',function(){
-      checkUser()
-    });   
-  },[])
-  //***setUser will update user with the user returned from the supabase***
-  async function checkUser(){
-    const user =supabase.auth.getUser();
-    setUser(user);
-  }
-  async function signInWithOAuth() { 
-    await supabase.auth.signInWithOAuth({provider:'github'});
-  }
-  async function signOut() {
-    await supabase.auth.signOut();
-    setUser(null);
-  }*/
-  }
-  supabase.auth.getUser() // get current user
-  supabase.auth.signOut() // sign out
-  supabase.auth.signInWithOAuth({
-    // sign in with github
-    provider: 'github',
-    options: {
-      redirectTo: `home page url /`,
-    },
-  })
 
   return (
     <div id={theme}>
--- src/modules/auth/features/Login/Login.tsx
+++ src/modules/auth/features/Login/Login.tsx
@@ -1,7 +1,21 @@
+import { supabase } from '@src/client'
 import CardBalance from '@src/modules/shared/components/Cards/Card-BALANCE/Card-balance'
+import { PATH } from '../../routes/paths'
+import GithubIcon from '@src/modules/shared/assets/icons/github'
 const Login = () => {
+  const location = window.location
+  console.log(location)
+  console.log(PATH.LOGIN,"path")
   async function signInWithGithub() {
-    console.log('hello ')
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'github',
+      options: {
+        redirectTo: `${location.origin}${PATH.LOGIN}`,
+      },
+    })
+    console.log(data)
+    
+    
   }
 
   return (
@@ -9,7 +23,11 @@ const Login = () => {
       <div className="login-module">
         <div className="login-module__card">
           <p className="login-module__card__title">Welcome</p>
-          <button onClick={signInWithGithub}>login btn</button>
+          <p className="login-module__card__description"> Login via your Github account to get started with our app </p>
+          <div className='button-container'>
+            <img src={GithubIcon} alt="GithubIcon" />
+            <button onClick={signInWithGithub}>Sign in with Github</button>
+          </div>
         </div>
       </div>
     </CardBalance>
