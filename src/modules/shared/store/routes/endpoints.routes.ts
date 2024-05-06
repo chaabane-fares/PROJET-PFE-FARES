5558c861e7ab50a8eb80cfbfa16b0292bf345d37
chaabane-fares <chaabanefares3@gmail.com>
2024-05-06 17:13:40 +0100

    i have changed the endpoint of getPullRequest

--- src/modules/shared/store/routes/endpoints.routes.ts
+++ src/modules/shared/store/routes/endpoints.routes.ts
@@ -1,7 +1,7 @@
 export const endpoints = {
   auth: {},
   getRepositories: 'user/repos',
-  getPullRequests: `repos/:user/:repo/pulls`,
+  getPullRequests: `repos/:chaabane-fares/:PROJET-PFE-FARES/pulls`,
   getPullRequestsCommits: `repos/:user/:repo/pulls/:ref/commits`,
   getOneCommits: `repos/:user/:repo/commits/:ref`,
   getOneFileContent: `repos/:user/:repo/contents/:path?ref=:ref`,
